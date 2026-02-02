2f6ea64927a30fbf0231222f7cb9a683ba24c3f4
guilhermesaverio <guilhermepereirasaverio2019@gmail.com>
2026-02-02 01:43:03 -0300

    Adiciona classe TodoList com menu de console integrando TodoService

--- src/TodoList.java
+++ src/TodoList.java
@@ -1,5 +1,50 @@
+import service.TodoService;
+import java.util.Scanner;
+
 public class TodoList {
     public static void main(String[] args) {
+        TodoService service = new TodoService();
+        Scanner sc = new Scanner(System.in);
+        int opcao;
 
+        do {
+            System.out.println("\n===== TODO LIST =====");
+            System.out.println("1 - Adicionar tarefa");
+            System.out.println("2 - Listar tarefas");
+            System.out.println("3 - Concluir tarefa");
+            System.out.println("4 - Remover tarefa");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = sc.nextInt();
+            sc.nextLine(); // limpar buffer
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Descrição da tarefa: ");
+                    String desc = sc.nextLine();
+                    service.adicionarTarefa(desc);
+                    break;
+                case 2:
+                    service.listarTarefas();
+                    break;
+                case 3:
+                    System.out.print("Número da tarefa para concluir: ");
+                    int c = sc.nextInt();
+                    service.concluirTarefa(c);
+                    break;
+                case 4:
+                    System.out.print("Número da tarefa para remover: ");
+                    int r = sc.nextInt();
+                    service.removerTarefa(r);
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while (opcao != 0);
+
+        sc.close();
     }
 }
