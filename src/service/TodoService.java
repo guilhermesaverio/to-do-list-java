a7327bcb1bde7066a53d8b366ad7126637e3c587
guilhermesaverio <guilhermepereirasaverio2019@gmail.com>
2026-02-02 01:37:03 -0300

    Adiciona classe TodoService para gerenciar a lista de tarefas
    Serviço responsável por gerenciar a lista de tarefas. Permite adicionar, listar, concluir e remover tarefas.

--- src/service/TodoService.java
+++ src/service/TodoService.java
@@ -1,5 +1,39 @@
 package service;
 
-public class TodoService {
+import model.Tarefa;
+import java.util.ArrayList;
+import java.util.List;
 
+public class TodoService {
+    private List<Tarefa> tarefas = new ArrayList<>();
+
+    public void adicionarTarefa(String descricao) {
+        tarefas.add(new Tarefa(descricao));
+    }
+
+    public void listarTarefas() {
+        if (tarefas.isEmpty()) {
+            System.out.println("Nenhuma tarefa cadastrada.");
+            return;
+        }
+        for (int i = 0; i < tarefas.size(); i++) {
+            System.out.println((i + 1) + ". " + tarefas.get(i));
+        }
+    }
+
+    public void concluirTarefa(int indice) {
+        if (indice < 1 || indice > tarefas.size()) {
+            System.out.println("Índice inválido!");
+            return;
+        }
+        tarefas.get(indice - 1).concluir();
+    }
+
+    public void removerTarefa(int indice) {
+        if (indice < 1 || indice > tarefas.size()) {
+            System.out.println("Índice inválido!");
+            return;
+        }
+        tarefas.remove(indice - 1);
+    }
 }
